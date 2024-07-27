2ed7bb027e4c8af23406026594aa6925f719df76
Cristian Romero <cristian.romerogarcia10@gmail.com>
2024-07-26 22:39:00 -0600

    Create a generic colletion view in Playlist ViewController to add information later

--- spotifyApp/Controllers/Other/PlaylistViewController.swift
+++ spotifyApp/Controllers/Other/PlaylistViewController.swift
@@ -11,6 +11,31 @@ class PlaylistViewController: UIViewController {
 
     private let playlist: Playlist
     
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _ , _ -> NSCollectionLayoutSection? in
+            //Item
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(80)),
+                    subitem: item,
+                    count: 1
+            )
+            //Section
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        })
+    )
+    
     init(playlist: Playlist) {
         self.playlist = playlist
         super.init(nibName: nil, bundle: nil)
@@ -25,6 +50,12 @@ class PlaylistViewController: UIViewController {
         title = playlist.name
         view.backgroundColor = .systemBackground
         
+        view.addSubview(collectionView)
+        collectionView.register(RecommendedTrackCollectionViewCell.self, forCellWithReuseIdentifier: RecommendedTrackCollectionViewCell.identifier)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         APICaller.shared.getPlayListDetails(for: playlist) { result in
             switch result {
             case .success(let model):
@@ -34,5 +65,29 @@ class PlaylistViewController: UIViewController {
             }
         }
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
 
+}
+
+extension PlaylistViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendedTrackCollectionViewCell.identifier, for: indexPath) as? RecommendedTrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+//        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+}
+extension PlaylistViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        //Play Song
+    }
 }
