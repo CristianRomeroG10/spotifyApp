0baa653594deb757ae8cac098208fe45ae1bb9c7
Cristian Romero <cristian.romerogarcia10@gmail.com>
2024-11-17 10:55:52 -0600

    Modified details in AlbumViewcontroller add AlbumsCollectionViewCellViewModel to data add album tracksViewController

---
+++ spotifyApp/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  spotifyApp
+//
+//  Created by Cristian guillermo Romero garcia on 17/11/24.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel {
+    let name : String
+    let artistName: String
+}
